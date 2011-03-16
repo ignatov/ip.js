f69b8945511c8b0698e65e401d16abe81564fae8
ignatov <sergey@ignatov.spb.su>
2011-03-16 23:19:52 +0300

    added Array.protorype.median

--- js/utils.js
+++ js/utils.js
@@ -12,3 +12,17 @@ String.prototype.replaceAll = function(searchValue, replaceValue) {
   }
   return temp;
 };
+
+Array.prototype.median = function () {
+  var ary = this.slice();
+  if (ary.length == 0)
+    return [];
+  ary.sort(function (a, b) {
+    return a - b
+  });
+  var mid = Math.floor(ary.length / 2);
+  if ((ary.length % 2) == 1)
+    return ary[mid];
+  else
+    return (ary[mid - 1] + ary[mid]) / 2;
+};
